a0bde83980e0f43e8a379e658b79b418d0fde290
abhi1994j <abhibaban4@gmail.com>
2025-04-20 03:27:37 +0530

    ui complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
   <>
-      <footer className="w-full py-4 px-2 bg-gray-50">
+      <footer className="w-full py-2 sm:py-4 px-2 bg-gray-50">
               <div className="flex flex-col md:flex-row justify-between items-center mx-2 mb-5">
                   <span className="text-xl font-semibold">Abhishek.</span>
                   <div className="text-[14px]  ltr:text-right text-gray-500">
